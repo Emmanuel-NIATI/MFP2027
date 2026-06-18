3165b84b2146244384963500f9f6c6aa7a60c617
Emmanuel NIATI <emmanuel.niati@outlook.fr>
2026-06-18 17:25:36 +0200

    V100 : Initialisation du Projet

--- 01_TM1637/test_tm1637.py
+++ 01_TM1637/test_tm1637.py
@@ -11,3 +11,15 @@ while True:
     sleep(200)
     pin8.write_digital(False)
     sleep(200)
+
+
+
+tm = TM1637(clk=pin1, dio=pin2, brightness=2)
+
+tm.show_binary([0b11111111,0b11111111,0b11111111,0b11111111,0b11111111,0b11111111])
+
+while True:
+    pin8.write_digital(True)
+    sleep(200)
+    pin8.write_digital(False)
+    sleep(200)
--- 01_TM1637/tm1637.py
+++ 01_TM1637/tm1637.py
@@ -399,13 +399,3 @@ class TM1637(object):
 
         self.show_data(encoded_data)
 
-
-tm = TM1637(clk=pin1, dio=pin2, brightness=2)
-
-tm.show_binary([0b11111111,0b11111111,0b11111111,0b11111111,0b11111111,0b11111111])
-
-while True:
-    pin8.write_digital(True)
-    sleep(200)
-    pin8.write_digital(False)
-    sleep(200)
